69f0b6259c8dbf36de130c611d8b5a63ec48315a
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2020-10-18 15:40:51 +0200

    Update config

--- src/config.py
+++ src/config.py
@@ -1,19 +1,7 @@
 database = dict(
-    db_connection_str = 'mysql+pymysql://msr14:haslo@localhost:3306/msr14'
+    db_con_str = 'mysql+pymysql://msr14:haslo@localhost:3306/msr14'
 )
 
 project_path = dict(
-
-)
-
-data_local_path = dict(
-    projects_db = 'data/01_data_from_db/projects.pkl',
-    commits_db = 'data/01_data_from_db/commits.pkl',
-    commits_comments_db = 'data/01_data_from_db/commits_comments.pkl',
-    issues_db = 'data/01_data_from_db/issues.pkl',
-    issues_comments_db = 'data/01_data_from_db/issues_comments.pkl',
-    pull_requests_db = 'data/01_data_from_db/pull_requests.pkl',
-    pull_requests_history_db = 'data/01_data_from_db/pull_requests_history_db.pkl',
-    pull_requests_comments_db = 'data/01_data_from_db/pull_requests_comments.pkl',
-    watchers_db = 'data/01_data_from_db/watchers.pkl'
+    ubuntu = '/home/maciejdudek/Pulpit/inżynierka'
 )
